4db81f0a72246357efca4f962263ceb8afe1974b
Thomas Sands <thomas_sands@adminstrator.local>
2022-11-09 22:51:39 -0800

    oops i submitted wrong version

--- 2022.11.09_character_creator.py
+++ 2022.11.09_character_creator.py
@@ -1,5 +1,7 @@
 #Welcome to Character Creator, with 157464 possible combinations!
 
+from random import randint
+
 from graphics import *
 from Button import *
 
@@ -39,17 +41,17 @@ character = {
     "head": {
         "size": 0,
         "shape": 0,
-        "color": 1
+        "color": 0
     },
     "nose": {
-        "size": 1,
-        "shape": 1,
-        "color": 3
+        "size": 0,
+        "shape": 0,
+        "color": 0
     },
     "eyes": {
-        "size": 2,
+        "size": 0,
         "shape": 0,
-        "color": 5
+        "color": 0
     }
 }
 
@@ -82,58 +84,53 @@ def main():
     save_button = Button(win, Point(600,450), Point(700,550), 5, "Save as .char", colors["grey"], colors["black"], colors["black"])
     load_button = Button(win, Point(725,450), Point(825,550), 5, "Load from .char", colors["grey"], colors["black"], colors["black"])
     quit_button = Button(win, Point(850,450), Point(950,550), 5, "Quit", colors["grey"], colors["black"], colors["black"])
+    #draw constant
+    color1 = [60, 60, 60]
+    p  = []
+    for i in range(600):
+        p.append(Line(Point(0,i), Point(1000,i)))
+        p[i].setFill(color_rgb(round(color1[0]), round(color1[1]), round(color1[2])))
+        p[i].draw(win)
+        color1[0] += 1/12
+        color1[1] += 1/12
+        color1[2] += 1/12
+    save_button.draw()
+    load_button.draw()
+    quit_button.draw()
+    #draw initial
+    head_size_button.editText(attribute_texts["head"]["size"][character["head"]["size"]])
+    head_shape_button.editText(attribute_texts["head"]["shape"][character["head"]["shape"]])
+    head_color_button.editText(attribute_texts["head"]["color"][character["head"]["color"]])
+    nose_size_button.editText(attribute_texts["nose"]["size"][character["nose"]["size"]])
+    nose_shape_button.editText(attribute_texts["nose"]["shape"][character["nose"]["shape"]])
+    nose_color_button.editText(attribute_texts["nose"]["color"][character["nose"]["color"]])
+    eyes_size_button.editText(attribute_texts["eyes"]["size"][character["eyes"]["size"]])
+    eyes_shape_button.editText(attribute_texts["eyes"]["shape"][character["eyes"]["shape"]])
+    eyes_color_button.editText(attribute_texts["eyes"]["color"][character["eyes"]["color"]])
+    head_size_button.undraw()
+    head_shape_button.undraw()
+    head_color_button.undraw()
+    nose_size_button.undraw()
+    nose_shape_button.undraw()
+    nose_color_button.undraw()
+    eyes_size_button.undraw()
+    eyes_shape_button.undraw()
+    eyes_color_button.undraw()
+    head_size_button.draw()
+    head_shape_button.draw()
+    head_color_button.draw()
+    nose_size_button.draw()
+    nose_shape_button.draw()
+    nose_color_button.draw()
+    eyes_size_button.draw()
+    eyes_shape_button.draw()
+    eyes_color_button.draw()
+    head = Rectangle(Point(250, 250), Point(350, 350))
+    head.setFill(color_list[0])
+    nose = Point(10, 10)
+    eye1 = Point(10, 10)
+    eye2 = Point(10, 10)
     while(True):
-        #draw background
-        color1 = [60, 60, 60]
-        p = []
-        for i in range(600):
-            p.append(Line(Point(0,i), Point(1000,i)))
-            p[i].setFill(color_rgb(round(color1[0]), round(color1[1]), round(color1[2])))
-            p[i].draw(win)
-            color1[0] += 1/12
-            color1[1] += 1/12
-            color1[2] += 1/12
-        #draw selection buttons
-        head_size_button.editText(attribute_texts["head"]["size"][character["head"]["size"]])
-        head_shape_button.editText(attribute_texts["head"]["shape"][character["head"]["shape"]])
-        head_color_button.editText(attribute_texts["head"]["color"][character["head"]["color"]])
-        nose_size_button.editText(attribute_texts["nose"]["size"][character["nose"]["size"]])
-        nose_shape_button.editText(attribute_texts["nose"]["shape"][character["nose"]["shape"]])
-        nose_color_button.editText(attribute_texts["nose"]["color"][character["nose"]["color"]])
-        eyes_size_button.editText(attribute_texts["eyes"]["size"][character["eyes"]["size"]])
-        eyes_shape_button.editText(attribute_texts["eyes"]["shape"][character["eyes"]["shape"]])
-        eyes_color_button.editText(attribute_texts["eyes"]["color"][character["eyes"]["color"]])
-        head_size_button.undraw()
-        head_shape_button.undraw()
-        head_color_button.undraw()
-        nose_size_button.undraw()
-        nose_shape_button.undraw()
-        nose_color_button.undraw()
-        eyes_size_button.undraw()
-        eyes_shape_button.undraw()
-        eyes_color_button.undraw()
-        head_size_button.draw()
-        head_shape_button.draw()
-        head_color_button.draw()
-        nose_size_button.draw()
-        nose_shape_button.draw()
-        nose_color_button.draw()
-        eyes_size_button.draw()
-        eyes_shape_button.draw()
-        eyes_color_button.draw()
-        #draw meta buttons
-        save_button.undraw()
-        load_button.undraw()
-        quit_button.undraw()
-        save_button.draw()
-        load_button.draw()
-        quit_button.draw()
-        #initializecharacter
-        head = Rectangle(Point(250, 250), Point(350, 350))
-        head.setFill(color_list[0])
-        nose = Point(10, 10)
-        eye1 = Point(10, 10)
-        eye2 = Point(10, 10)
         #drawcharacter
         head.undraw()
         nose.undraw()
@@ -227,56 +224,90 @@ def main():
                     character["head"]["size"] = 0
                 else:
                     character["head"]["size"] += 1
+                head_size_button.editText(attribute_texts["head"]["size"][character["head"]["size"]])
+                head_size_button.undraw()
+                head_size_button.draw()
             elif(725 < click.getX() and click.getX() < 825):
                 if(character["head"]["shape"] == 1):
                     character["head"]["shape"] = 0
                 else:
                     character["head"]["shape"] += 1
+                head_shape_button.editText(attribute_texts["head"]["shape"][character["head"]["shape"]])
+                head_shape_button.undraw()
+                head_shape_button.draw()
             elif(850 < click.getX() and click.getX() < 950):
                 if(character["head"]["color"] == 5):
                     character["head"]["color"] = 0
                 else:
                     character["head"]["color"] += 1
+                head_color_button.editText(attribute_texts["head"]["color"][character["head"]["color"]])
+                head_color_button.undraw()
+                head_color_button.draw()
         elif(175 < click.getY() and click.getY() < 275):
             if(600 < click.getX() and click.getX() < 700):
                 if(character["nose"]["size"] == 2):
                     character["nose"]["size"] = 0
                 else:
                     character["nose"]["size"] += 1
+                nose_size_button.editText(attribute_texts["nose"]["size"][character["nose"]["size"]])
+                nose_size_button.undraw()
+                nose_size_button.draw()
             elif(725 < click.getX() and click.getX() < 825):
                 if(character["nose"]["shape"] == 1):
                     character["nose"]["shape"] = 0
                 else:
                     character["nose"]["shape"] += 1
+                nose_shape_button.editText(attribute_texts["nose"]["shape"][character["nose"]["shape"]])
+                nose_shape_button.undraw()
+                nose_shape_button.draw()
             elif(850 < click.getX() and click.getX() < 950):
                 if(character["nose"]["color"] == 5):
                     character["nose"]["color"] = 0
                 else:
                     character["nose"]["color"] += 1
+                nose_color_button.editText(attribute_texts["nose"]["color"][character["nose"]["color"]])
+                nose_color_button.undraw()
+                nose_color_button.draw()
         elif(300 < click.getY() and click.getY() < 400):
             if(600 < click.getX() and click.getX() < 700):
                 if(character["eyes"]["size"] == 2):
                     character["eyes"]["size"] = 0
                 else:
                     character["eyes"]["size"] += 1
+                eyes_size_button.editText(attribute_texts["eyes"]["size"][character["eyes"]["size"]])
+                eyes_size_button.undraw()
+                eyes_size_button.draw()
             elif(725 < click.getX() and click.getX() < 825):
                 if(character["eyes"]["shape"] == 1):
                     character["eyes"]["shape"] = 0
                 else:
                     character["eyes"]["shape"] += 1
+                eyes_shape_button.editText(attribute_texts["eyes"]["shape"][character["eyes"]["shape"]])
+                eyes_shape_button.undraw()
+                eyes_shape_button.draw()
             elif(850 < click.getX() and click.getX() < 950):
                 if(character["eyes"]["color"] == 5):
                     character["eyes"]["color"] = 0
                 else:
                     character["eyes"]["color"] += 1
+                eyes_color_button.editText(attribute_texts["eyes"]["color"][character["eyes"]["color"]])
+                eyes_color_button.undraw()
+                eyes_color_button.draw()
         elif(450 < click.getY() and click.getY() < 550):
             if(600 < click.getX() and click.getX() < 700):#save
-                file = open("download.char", "x")
+                code = randint(0,999999)
+                file = open(f"character_download_{code}.char", "x")
+                file = open(f"character_download_{code}.char", "w")
                 file.write(getsavecode())
-                print("File saved as download.char")
+                file.close()
+                print(f"File saved as character_download_{code}.char")
             elif(725 < click.getX() and click.getX() < 825):#load
                 file = input("Enter file to load: ")
-                
+                if(file[-5:] != ".char"):
+                    print("Unable to read file")
+                else:
+                    file = open(file)
+                    loadsavecode(file.read())
             elif(850 < click.getX() and click.getX() < 950):
                 win.close()
                 exit()
